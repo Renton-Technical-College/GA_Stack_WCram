27cd98916efa0dfb4c1257c397c8aeaa195f8584
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-30 01:50:53 +0000

    update

--- Program.cs
+++ Program.cs
@@ -4,9 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Stack<string> builtInStack = new Stack<string>();
+            Stack<int> myStack = new Stack<int>();
 
-            
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+
+            int poppedItem = myStack.Pop();
+
+            Console.WriteLine("Popped Element: " + poppedItem);
 
         } // Main
 
